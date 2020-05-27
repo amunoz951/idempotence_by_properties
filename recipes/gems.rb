ece045a29bbff911eef5fcfae0783b5c75a851e4
Munoz, Alex <Alex.Munoz@ncr.com>
2020-05-27 11:25:29 -0700

    Use specific gem versions since Chef won't support fuzzy versioning until 16.2

--- recipes/gems.rb
+++ recipes/gems.rb
@@ -7,20 +7,20 @@
 #
 
 required_gems = {
-  'os' => '~> 1.1',
-  'hashly' => '~> 0.1', # dependency of easy_json_config
-  'easy_json_config' => '~> 0.3', # dependency of easy_io
-  'logger' => '~> 1.4', # dependency of easy_io
-  'open3' => '~> 0.1', # dependency of easy_io
-  'easy_format' => '~> 0.2', # dependency of easy_io
-  'easy_io' => '~> 0.4',
-  'easy_state' => '~> 0.1',
+  'os' => '1.1.0',
+  'hashly' => '0.1.0', # dependency of easy_json_config
+  'easy_json_config' => '0.3.0', # dependency of easy_io
+  'logger' => '1.4.2', # dependency of easy_io
+  'open3' => '0.1.0', # dependency of easy_io
+  'easy_format' => '0.2.0', # dependency of easy_io gem
+  'easy_io' => '0.4.1', # dependency of zipr gem
+  'easy_state' => '0.1.0',
 }
 required_gems.each do |gem_name, gem_version|
   chef_gem gem_name do
     version gem_version
     compile_time true
-    action :upgrade
+    action :install
   end
 end
 
