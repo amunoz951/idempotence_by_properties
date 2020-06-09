9ab2ef85c84e1bbe2904eb49592dffad65664b9d
Munoz, Alex <Alex.Munoz@ncr.com>
2020-06-09 10:33:33 -0700

    Add simple kitchen test

--- test/cookbooks/idempotence_by_properties_test/recipes/default.rb
+++ test/cookbooks/idempotence_by_properties_test/recipes/default.rb
@@ -3,3 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2020, Alex Munoz, All Rights Reserved.
+
+idempotence_by_properties do
+  execute 'This is a test' do
+    command 'echo This is a test message.'
+  end
+end
